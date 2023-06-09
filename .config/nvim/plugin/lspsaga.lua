38004153081aafc413480cfa5f9021d71c108a12
Alexander Heinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-06-09 19:39:55 +0200

    cleaned up saga by removing unused shortcuts and refactoring existing ones

--- .config/nvim/plugin/lspsaga.lua
+++ .config/nvim/plugin/lspsaga.lua
@@ -11,20 +11,14 @@ saga.setup({
   }
 })
 
-local diagnostic = require("lspsaga.diagnostic")
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', diagnostic.goto_next, opts)
---vim.keymap.set('n', 'gl', diagnostic.show_diagnostics, opts)
+vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 
 -- code action
-local codeaction = require("lspsaga.codeaction")
-vim.keymap.set("n", "ca", function() codeaction:code_action() end, { silent = true })
-vim.keymap.set("v", "ca", function()
-  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
-  codeaction:range_code_action()
-end, { silent = true })
+vim.keymap.set({"n", "v"}, "ca", "<Cmd>Lspsaga code_action<CR>")
